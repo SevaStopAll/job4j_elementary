5246c8165b6e7ae97377475ca84d884321ff1cbe
SevaStopAll <Sevastopallrh@yandex.ru>
2022-11-28 16:11:29 +0400

    4.2. Заголовок в файле java [#159036 #365240]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Vsevolod Ososkov");
+        System.out.println("Vsevolod");
         System.out.println("22.01.1994");
     }
 }
