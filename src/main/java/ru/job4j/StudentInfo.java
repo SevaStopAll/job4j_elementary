ef64f4d4630375927e941b373307c25d0e9f1f9c
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-11-28 13:43:34 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String [] args) {
+        System.out.println("Vsevolod Ososkov");
+        System.out.println("22.01.1994");
+    }
+}
