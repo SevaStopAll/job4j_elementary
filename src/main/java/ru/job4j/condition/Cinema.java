d2e971d3efa1b9d400ec938d7aced49d9f1fc987
SevaStopAll <Sevastopallrh@yandex.ru>
2022-11-29 21:52:32 +0400

    4.1.0. Оператор if с блоком else [#227534]

---
+++ src/main/java/ru/job4j/condition/Cinema.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Cinema {
+    public static void access(int age) {
+        System.out.println("The age of the customer is : " + age);
+        if (age > 17) {
+            System.out.println("Welcome to the cinema.");
+        } else {
+            System.out.println("It is not for you.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cinema.access(21);
+        Cinema.access(16);
+        Cinema.access(18);
+
+    }
+}
