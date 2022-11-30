43b29f13281616f18b9e3d764891d3f525ff6889
SevaStopAll <Sevastopallrh@yandex.ru>
2022-11-30 20:15:55 +0400

    5.4. Протеиновая диета (цикл while) [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+}
