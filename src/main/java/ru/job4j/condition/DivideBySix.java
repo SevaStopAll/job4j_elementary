4331503591640013cec5e8083f9ac64807f20f1e
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-11-30 10:41:00 +0400

    4.4. Вложенный оператор if else. Запутанный код [#190]

---
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class DivideBySix {
+    public static String checkNumber(int number) {
+        String rsl;
+        if (number % 2 == 0 && number % 3 == 0) {
+            rsl = "The number divides by 6.";
+        } else if (number % 2 != 0 && number % 3 == 0) {
+            rsl = "The number divides by 3, but it isn't the even number.";
+        } else if (number % 2 == 0 && number % 3 != 0) {
+            rsl = "The number doesn't divide by 3, but it is the even number.";
+        } else {
+            rsl = "The number doesn't divide by 3 and it isn't the even number.";
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/condition/DivideBySixTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DivideBySixTest {
+    @Test
+    public void whenNumberDivideBy6() {
+        int in = 24;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "The number divides by 6.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberDivideBy3AndNotEven() {
+        int in = 9;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "The number divides by 3, but it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberNoDivideBy3AndEven() {
+        int in = 14;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "The number doesn't divide by 3, but it is the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberNoDivideBy3AndNotEven() {
+        int in = 25;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "The number doesn't divide by 3 and it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
+}
