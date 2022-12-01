f831e5aff44d8b5321e68a042d6114ed872bbcc4
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-01 08:53:46 +0400

    6.2.3. Перевернуть массив. [#226790]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 -i] = temp;
+        }
+        return array;
+    }
+}
---
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {2, 6, 1, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int[] result =  Turn.back(input);
+        int[] expected = new int[] {7, 6, 5, 4, 3, 2, 1};
+        assertThat(result).containsExactly(expected);
+    }
+}
