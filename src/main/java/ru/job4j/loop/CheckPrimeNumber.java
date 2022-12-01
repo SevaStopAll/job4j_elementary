e6f925d45e76610d9e08254de922b7dc8db93611
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-01 14:35:12 +0400

    5.5. Простое число [#171694 #366201]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,17 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            return false;
-        } else {
+        boolean prime = number > 1;
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
                     prime = false;
                     break;
                 }
             }
-        }
         return prime;
     }
 }
