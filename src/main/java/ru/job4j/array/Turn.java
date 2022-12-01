e2818258a7565cfe5272605f3fcb23041d32875e
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-01 09:01:59 +0400

    6.2.3. Перевернуть массив. [#226790]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -5,7 +5,7 @@ public class Turn {
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
-            array[array.length - 1 -i] = temp;
+            array[array.length - 1 - i] = temp;
         }
         return array;
     }
--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -14,9 +14,9 @@ public class TurnTest {
 
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] result =  Turn.back(input);
-        int[] expected = new int[] {7, 6, 5, 4, 3, 2, 1};
+        int[] expected = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1};
         assertThat(result).containsExactly(expected);
     }
 }
