6740e04b27c55fd7d3b1b7c3217a69ed3a282149
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-01 09:21:41 +0400

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
