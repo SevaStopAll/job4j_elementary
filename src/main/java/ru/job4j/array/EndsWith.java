ef85964f495178190946f6dae71b009e0d99986b
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-01 21:38:25 +0400

    6.4.3. Слово заканчивается на ... [#156317 #366381]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        for (int index = 1; index < post.length; index++) {
-            if (word[word.length - index] != post[post.length - index]) {
+        for (int index = 0; index < post.length; index++) {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
                 return false;
             }
         }
