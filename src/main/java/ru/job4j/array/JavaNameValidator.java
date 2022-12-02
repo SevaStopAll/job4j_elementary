a778485217da54d60f46f2fd8e91e2e6aed6e2fa
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-02 12:06:23 +0400

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name.isEmpty() || !Character.isLowerCase(name.charAt(0))) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            if (!isSpecialSymbol(name.codePointAt(i)) && !isUpperLatinLetter(name.codePointAt(i))
+                    && !isLowerLatinLetter(name.codePointAt(i)) && !Character.isDigit(name.codePointAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36
+                || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
